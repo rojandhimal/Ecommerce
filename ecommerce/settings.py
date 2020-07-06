31e5e13e61369a9ee2bb688de697346a114fb04c
rojandhimal <Rojandhimal1@gmail.com>
2020-07-06 14:32:47 +0545

    Updating static path

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'ecommerce',
+    'ecommerce',
 
     'store.apps.StoreConfig',
 ]
@@ -121,10 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
 ]
+MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/')
