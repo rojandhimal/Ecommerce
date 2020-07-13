4b8fb763dfd839d324e7171bcf02599ca196ed6e
rojandhimal <Rojandhimal1@gmail.com>
2020-07-13 22:47:05 +0545

    update database field

--- store/models.py
+++ store/models.py
@@ -14,7 +14,7 @@ class Customer(models.Model):
 
 class Product(models.Model):
 	name = models.CharField(max_length=200)
-	price = models.DecimalField(max_digits=10, decimal_places=2)
+	price = models.FloatField()
 	digital = models.BooleanField(default=False,null=True, blank=True)
 	image = models.ImageField(null=True, blank=True)
 
@@ -37,7 +37,7 @@ class Order(models.Model):
 
 	def __str__(self):
 		return str(self.id)
-
+		
 	@property
 	def shipping(self):
 		shipping = False
